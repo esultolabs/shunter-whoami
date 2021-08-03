82607f2777bc2fb4d4d5deea95b2d4b794253f46
Sgobbi Federico <federico@sgobbi.it>
2021-08-03 09:03:57 +0200

    BUGFIX container don't start

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,22 +13,33 @@
  *    Import externals
  */
 const express = require('express');
+const fs = require('fs');
 const os = require('os');
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *    The code
  */
+var nodeHostname = null;
+if (fs.existsSync('/data/node-hostname')) {
+  nodeHostname = fs.readFileSync('/data/node-hostname', 'utf8').replace(/(\r\n|\n|\r)/gm, "").trim();
+}
+
 const router = express.Router();
 
 router.get('/', function (req, res) {
   let info = {
     domain: req.headers.host,
-    containerID: os.hostname(),
+    // Real node hostname
+    nodeHostmane: nodeHostname,
+    // Container info
+    containerHostname: os.hostname(),
     containerAddress: req.socket.localAddress,
     containerPort: req.socket.localPort,
+    // IP & port of reverse proxy
     remoteAddress: req.socket.remoteAddress,
     remotePort: req.socket.remotePort,
+    // Real client IP of forwarded request from reverse proxy
     forwardedIP: req.headers['x-forwarded-for'] || null
   }
 
